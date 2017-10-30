adf30ea25784ec21863fc8186d4a326e92fdddbb
CrazyMerlyn <crazy.d.merlyn@gmail.com>
2017-10-30 18:58:30 +0530

    Implement serializable for stack map frame

--- src/attribute.rs
+++ src/attribute.rs
@@ -21,6 +21,7 @@ impl Serializable for Attribute {
 pub enum AttributeType {
     Constant(u16),
     Code(CodeAttribute),
+    StackMapTable(Vec<StackMapFrame>),
 }
 
 impl Serializable for AttributeType {
@@ -28,6 +29,7 @@ impl Serializable for AttributeType {
         match *self {
             AttributeType::Constant(index) => index.serialize(out),
             AttributeType::Code(ref code) => code.serialize(out),
+            AttributeType::StackMapTable(ref entries) => entries.serialize(out),
         }
     }
 }
@@ -47,5 +49,102 @@ impl Serializable for CodeAttribute {
         self.code.serialize(out)?;
         self.exceptions.serialize(out)?;
         self.attributes.serialize(out)
+    }
+}
+
+pub enum TypeInfo {
+    TopVariable,
+    Integer,
+    Float,
+    Long,
+    Double,
+    Null,
+    UninitializedThis,
+    Object(u16),
+    Uninitialized(u16),
+}
+
+const TOP_VARIABLE_INFO: u8 = 0;
+const INTEGER_VARIABLE_INFO: u8 = 1;
+const FLOAT_VARIABLE_INFO: u8 = 2;
+const LONG_VARIABLE_INFO: u8 = 4;
+const DOUBLE_VARIABLE_INFO: u8 = 3;
+const NULL_VARIABLE_INFO: u8 = 5;
+const UNINITIALIZED_THIS_VARIABLE_INFO: u8 = 6;
+const OBJECT_VARIABLE_INFO: u8 = 7;
+const UNINITIALIZED_VARIABLE_INFO: u8 = 8;
+
+impl Serializable for TypeInfo {
+    fn serialize<W: Write>(&self, out: &mut W) -> Result<()> {
+        match *self {
+            TypeInfo::TopVariable => TOP_VARIABLE_INFO.serialize(out),
+            TypeInfo::Integer => INTEGER_VARIABLE_INFO.serialize(out),
+            TypeInfo::Float => FLOAT_VARIABLE_INFO.serialize(out),
+            TypeInfo::Long => LONG_VARIABLE_INFO.serialize(out),
+            TypeInfo::Double => DOUBLE_VARIABLE_INFO.serialize(out),
+            TypeInfo::Null => NULL_VARIABLE_INFO.serialize(out),
+            TypeInfo::UninitializedThis => UNINITIALIZED_THIS_VARIABLE_INFO.serialize(out),
+            TypeInfo::Object(index) => {
+                OBJECT_VARIABLE_INFO.serialize(out)?;
+                index.serialize(out)
+            }
+            TypeInfo::Uninitialized(offset) => {
+                UNINITIALIZED_VARIABLE_INFO.serialize(out)?;
+                offset.serialize(out)
+            }
+        }
+    }
+}
+
+pub enum StackMapFrame {
+    Same,
+    SameLocals1StackItem(u8, TypeInfo),
+    SameLocals1StackItemExtended(u16, TypeInfo),
+    Chop(u8, u16),
+    SameExtended(u16),
+    Append(u16, Vec<TypeInfo>),
+    Full(u16, Vec<TypeInfo>, Vec<TypeInfo>),
+}
+
+const EXTENDED_STACK_ITEM_TAG: u8 = 247;
+const SAME_FRAME_TAG: u8 = 251;
+const FULL_FRAME_TAG: u8 = 255;
+
+impl Serializable for StackMapFrame {
+    fn serialize<W: Write>(&self, out: &mut W) -> Result<()> {
+        match *self {
+            StackMapFrame::Same => 0u8.serialize(out),
+            StackMapFrame::SameLocals1StackItem(ty, ref info) => {
+                ty.serialize(out)?;
+                info.serialize(out)
+            }
+            StackMapFrame::SameLocals1StackItemExtended(offset, ref info) => {
+                EXTENDED_STACK_ITEM_TAG.serialize(out)?;
+                offset.serialize(out)?;
+                info.serialize(out)
+            }
+            StackMapFrame::Chop(n_vars, offset) => {
+                (SAME_FRAME_TAG - n_vars).serialize(out)?;
+                offset.serialize(out)
+            }
+            StackMapFrame::SameExtended(offset) => {
+                SAME_FRAME_TAG.serialize(out)?;
+                offset.serialize(out)
+            }
+            StackMapFrame::Append(offset, ref infos) => {
+                (SAME_FRAME_TAG + infos.len() as u8).serialize(out)?;
+                offset.serialize(out)?;
+                for info in infos {
+                    info.serialize(out)?;
+                }
+                Ok(())
+            }
+            StackMapFrame::Full(offset, ref locals, ref stack) => {
+                FULL_FRAME_TAG.serialize(out)?;
+                offset.serialize(out)?;
+                locals.serialize(out)?;
+                stack.serialize(out)
+            }
+        }
     }
 }
